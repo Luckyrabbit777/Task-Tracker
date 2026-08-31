3e6cdc8f57f74c2fc337c882d62453743bf29c1e
Anshuman Pratap <shivam.rajput2316w@gmail.com>
2026-08-31 18:02:45 +0530

    created the skeleton of the TaskService Class

--- Model/Tasks.java
+++ Model/Tasks.java
@@ -7,4 +7,14 @@ public class Tasks {
     boolean done;
     int createdAt;
     int updatedAt;
+
+    public Tasks(String taskName, int id, String Description, String status, boolean done, int createdAt, int updatedAt) {
+        this.taskName = taskName;
+        this.id = id;
+        this.Description = Description;
+        this.status = status;
+        this.done = done;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
--- Service/TaskService.java
+++ Service/TaskService.java
@@ -1,4 +1,26 @@
 package Service;
+import Storage.TasksStorage;
 public class TaskService {
-    
+
+    public void addTasks(){
+
+    }
+
+    public void delteTask(){
+    }
+    public void editTasks(){
+    }
+    public void deleteAllTasks(){
+
+    }
+    public void ListAllTasks(){
+    }
+
+    public void ListAllTasksAreDone(){
+
+    }
+    public void ListAllTasksAreNotDone(){
+    }
+    public void ListTasksInProgress(){
+    }
 }
